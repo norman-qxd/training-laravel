35db47947c03c003dc6ea30041d4e9acba2a1e66
norman-qxd <nbetancourtt@qxdev.com>
2023-07-11 16:28:54 -0600

    adsasd

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,14 +9,14 @@ use Illuminate\Http\Request;
 class PostsController extends Controller
 {
     public function index(){
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all()
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
+            'currentCategory' => Category::firstWhere('slug', request('category'))
         ]);
     }
 
     public function show(Post $post){
-        return view('post', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -6,36 +6,5 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [PostsController::class, 'index'])->name('home');
 Route::get('post/{post}', [PostsController::class, 'show']);
-
-
-Route::get('categories/{category:slug}', function (Category $category){
-    return view('posts', [
-        'posts' =>  $category->posts,
-        'currentCategory' =>  $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
-
-Route::get('authors/{author:username}', function (User $author){
-    return view('posts', [
-        'posts' =>  $author->posts,
-        'categories' => Category::all()
-    ]);
-});
-
-
-
